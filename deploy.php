877d5fc84267ee2e02a2f48ee41bf2aa5db4d528
Bruno Dilhof <brunodilhof@gmail.com>
2026-02-12 23:47:11 +0100

    Work in progress

--- deploy.php
+++ deploy.php
@@ -5,6 +5,7 @@ namespace Deployer;
 require 'recipe/laravel.php';
 
 set('repository', 'git@github.com:bdilhof/app-spending.git');
+set('keep_releases', 3);
 
 host('production')
     ->setHostname('178.128.181.104')
@@ -13,3 +14,5 @@ host('production')
     ->set('branch', 'main');
 
 after('deploy:failed', 'deploy:unlock');
+after('deploy:symlink', 'build');
+after('deploy', 'artisan:cache:clear');
