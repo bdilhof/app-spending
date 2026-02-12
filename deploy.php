cd384918ab8975f83cc323a63edf5432cd419bc0
Bruno Dilhof <brunodilhof@gmail.com>
2026-02-12 22:43:14 +0100

    Work in progress

--- deploy.php
+++ deploy.php
@@ -4,22 +4,12 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
-// Config
-
 set('repository', 'git@github.com:bdilhof/app-spending.git');
-
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
-
-// Hosts
 
 host('production')
     ->setHostname('178.128.181.104')
     ->setRemoteUser('deployer')
-    ->setDeployPath('~/app-spending')
+    ->setDeployPath('/var/www/html/app-spending')
     ->set('branch', 'main');
-
-// Hooks
 
 after('deploy:failed', 'deploy:unlock');
