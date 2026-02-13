21ec382850660ed9422dead815f8288a918569d5
Bruno Dilhof <brunodilhof@gmail.com>
2026-02-13 01:06:49 +0100

    Work in progress

--- deploy.php
+++ deploy.php
@@ -13,6 +13,18 @@ host('production')
     ->setDeployPath('/var/www/html/app-spending')
     ->set('branch', 'main');
 
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:config:cache',
+    'artisan:route:cache',
+    'artisan:view:cache',
+    'artisan:event:cache',
+    // 'artisan:migrate',
+    'deploy:publish',
+]);
+
 after('deploy:failed', 'deploy:unlock');
-after('deploy:symlink', 'build');
-after('deploy', 'artisan:cache:clear');
+// after('deploy:symlink', 'build');
+// after('deploy', 'artisan:cache:clear');
