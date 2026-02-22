522d216ca6056510debc3ccc6f4bb99eea950b1b
Bruno Dilhof <brunodilhof@gmail.com>
2026-02-22 18:21:35 +0100

    Work in progress

---
+++ resources/views/livewire/components/month-overview.blade.php
@@ -0,0 +1,9 @@
+<div class="bg-white p-4">
+    <div class="vstack gap-4">
+        <h4 class="text-primary m-0">Tento mesiac</h4>
+        <div>
+            <p class="m-0">Výdavky celkom: <b>{{ formatCurrency($total) }}</b></p>
+            <p class="m-0">Márnosti: <b>{{ formatCurrency($discrepancies) }}</b></p>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/pages/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard.blade.php
@@ -94,7 +94,7 @@
                     @endif
                     @if($spends->isNotEmpty())
                         @foreach($spends as $date => $itemsByDate)
-                        <table class="table align-middle table-sm table-hover">
+                        <table class="table align-middle table-sm table-hover m-0">
                             <thead class="">
                                 <tr>
                                     <th>{{ humanDate($date) }}</th>
@@ -128,15 +128,8 @@
         <div class="col-lg-4">
 
             <!-- OVERVIEW -->
-            <div class="bg-white p-4">
-                <div class="vstack gap-4">
-                    <h4 class="text-primary m-0">Tento mesiac</h4>
-                    <div>
-                        <p class="m-0">Výdavky celkom: <b>{{ formatCurrency($this->spends->flatten()->sum('amount')) }}</b></p>
-                        <p class="m-0">Márnosti: <b>{{ formatCurrency($this->spends->flatten()->where('is_discretionary', true)->sum('amount')) }}</b></p>
-                    </div>
-                </div>
-            </div>
+            <livewire:components.month-overview />
+
         </div>
     </div>
 </div>
